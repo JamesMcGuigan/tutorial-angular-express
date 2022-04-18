ed427f939f53a1da7705ed6e353ee7d9587ec39d
JamesMcGuigan <james.mcguigan.github@gmail.com>
2022-04-19 00:24:36 +0100

    angular html data binding to node express server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
+
+
 export class AppComponent {
-  title = 'oracle-eye';
+  message = 'oracle-eye';
+  name  = ''
+
+  // DOCS: https://stackoverflow.com/questions/34802813/how-to-make-ajax-call-with-angular2ts
+  constructor(public http: Http) {
+    this.onChange();
+  }
+
+  onChange() {
+    this.http.get(`/api/hello?name=${this.name}`)
+      .subscribe(response => {
+        this.message = response.json().message;
+      });        
+  }
+
+  // DOCS: https://angular.io/guide/two-way-binding
+  onUpdateName(name: string) {
+    this.name = name;
+    this.onChange()
+  }
 }
+
+
+
