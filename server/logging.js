746f3adf6f445a60c214476b8f8f0068eb3a919d
JamesMcGuigan <james.mcguigan.github@gmail.com>
2022-04-20 19:47:52 +0100

    server | mkdir ./logs/; on init

--- server/logging.js
+++ server/logging.js
@@ -8,13 +8,29 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 const fs           = require('fs');
 const morgan       = require('morgan');
+const path         = require('path');
 const errorHandler = require('express-error-handler');
 const config       = require('./config.js')[process.env.NODE_ENV];
 
+// $ mkdir ./logs/
+function createLogDirectory() {
+  let logfiles = [ config.access_log, config.error_log, config.debug_log ];
+  let dirnames = [ ...new Set(logfiles.map(path.dirname)) ];
+  dirnames
+    .map(dirname     => path.resolve(dirname))
+    .filter(dirname  => !fs.existsSync(dirname))
+    .forEach(dirname => {
+      fs.mkdirSync(dirname, { recursive: true })
+      console.info(`mkdir: ${dirname}`)
+    })
+  ;
+}
+
 module.exports = function(app) {
-    var access_log_stream = fs.createWriteStream(config.access_log, {flags: 'a'});
-    var error_log_stream  = fs.createWriteStream(config.error_log,  {flags: 'a'});
-    
+    createLogDirectory();
+    let access_log_stream = fs.createWriteStream(config.access_log, {flags: 'a'});
+    let error_log_stream  = fs.createWriteStream(config.error_log,  {flags: 'a'});
+
     // Logging
     if( ["staging","production"].indexOf(process.env.NODE_ENV) != -1 ) {
         // TODO: http://learnboost.github.io/cluster/docs/logger.html
@@ -27,7 +43,7 @@ module.exports = function(app) {
         app.use(morgan("dev", {stream: access_log_stream }));
         app.use(errorHandler({showStack: true, dumpExceptions: true}));
     }
-    
+
     app.use(function(request, response, next) {
         response.header('Vary', 'Accept-Encoding');            // Instruct proxies to store both compressed and uncompressed versions
         response.header('Access-Control-Allow-Origin', '*');   // Allow connections from other hosts
